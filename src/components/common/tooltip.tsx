233bc75a09d78fe7825d6dae83ee43e6ed5cabb8
pham.duy <pham.duy@dena.jp>
2023-11-05 13:33:12 +0700

    Update tooltip component

--- src/components/common/tooltip.tsx
+++ src/components/common/tooltip.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 import classNames from 'classnames'
 
-import useDevice from '@/hooks/use-device'
 import styles from '@/styles/modules/tooltip.module.scss'
 
 type Props = {
@@ -13,50 +12,19 @@ type Props = {
 }
 
 const Tooltip: React.FC<Props> = ({ children, content, className, direction = 'bottom' }) => {
-  const { isMobile, isTablet, isPc } = useDevice()
   const tooltipRef = useRef<HTMLDivElement | null>(null)
   const [isTooltipVisible, setIsTooltipVisible] = useState<boolean>(false)
 
-  const handleMouseEnter = () => {
-    if (isPc) setIsTooltipVisible(true)
-  }
-
-  const handleMouseLeave = () => {
-    if (isPc) setIsTooltipVisible(false)
-  }
-
-  useEffect(() => {
-    const handleClick = () => {
-      if (isMobile || isTablet) setIsTooltipVisible(!isTooltipVisible)
-    }
-
-    if (isMobile || isTablet) tooltipRef.current?.addEventListener('click', handleClick)
-
-    return () => {
-      if (isMobile || isTablet) tooltipRef.current?.removeEventListener('click', handleClick)
-    }
-  }, [isMobile, isTablet, isTooltipVisible])
-
   return (
-    <div className={classNames(styles.wrapper, className)} ref={tooltipRef} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      <div className={styles.tooltip}>
-        {children}
-        {isTooltipVisible && (
-          <>
-            <div
-              className={classNames(styles.tooltip__arrow, [styles[direction]], {
-                [styles.active]: isTooltipVisible
-              })}
-            />
-            <div
-              className={classNames(styles.tooltip__content, [styles[direction]], {
-                [styles.active]: isTooltipVisible
-              })}
-            >
-              {content}
-            </div>
-          </>
-        )}
+    <div className={classNames(styles.wrapper, className)} ref={tooltipRef}>
+      <div className={classNames(styles.tooltip, { [styles.active]: isTooltipVisible })}>
+        <div onClick={() => setIsTooltipVisible(!isTooltipVisible)} className={styles.tooltip__icon}>
+          {children}
+        </div>
+        <div className={styles.tooltip__body}>
+          <div className={classNames(styles.tooltip__arrow, [styles[direction]])} />
+          <div className={classNames(styles.tooltip__content, [styles[direction]])}>{content}</div>
+        </div>
       </div>
     </div>
   )
