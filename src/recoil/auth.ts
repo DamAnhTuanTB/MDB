50dff21cd049e96382a6a108b5392625383279b4
pham.duy <pham.duy@dena.jp>
2023-11-07 20:13:10 +0700

    Add auth stage

---
+++ src/recoil/auth.ts
@@ -0,0 +1,20 @@
+import { atom, useRecoilState } from 'recoil'
+
+import { AccountInformation } from '@/types/account/information'
+
+export const isLoggedInStage = atom<boolean>({
+  key: 'isLoggedInStage',
+  default: false
+})
+
+export const profileStage = atom<AccountInformation | undefined>({
+  key: 'profileStage',
+  default: undefined
+})
+
+export const useAuthStore = () => {
+  const [isLoggedIn, setIsLoggedIn] = useRecoilState(isLoggedInStage)
+  const [profile, setProfile] = useRecoilState(profileStage)
+
+  return { isLoggedIn, setIsLoggedIn, profile, setProfile }
+}
